708afbd86bb7475829213c70040cbb007aaa8c2e
famnas321 <famnaspk380@gmail.com>
2025-02-05 21:10:49 +0530

    committed on 5.2.2025

--- server/Controller/authController.js
+++ server/Controller/authController.js
@@ -1,11 +1,22 @@
 import Agency from "../model/agencyModel"
 
 
-const registerAgency = (req,res)=>{
+const registerAgency = async (req,res)=>{
       const {companyName,email,password,confirmPassword,contactNumber,nameOfManager,registrationId,
-        contry,state,district
-      }=req.body
+        contry,state,district} = req.body
+    try{
 
+        
+   
+        const agency= new Agency({
+          con
+        })
+  
+     
+    }catch(error){
+
+    }
+   
       
 }
 export default registerAgency
